36d560a2625b53159bfb1b5e8f5e3fc3617ecb3b
tthopate <tanmaya.thopate@ensono.com>
2022-03-01 16:31:42 +0530

    Initial

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='pgpackup',
+    version='0.1.0',
+    author='Tanmaya T',
+    author_email='tanmayaT@random.com',
+    description='A utility for backing up PostgreSQL databases',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/tanashwi/pgbackup',
+    packages=find_packages('src')
+)
